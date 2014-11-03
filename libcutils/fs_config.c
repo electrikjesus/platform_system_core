e47c34549088806ed151666c79594891978715cf
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-11-03 23:06:02 +0800

    Allow all to execute su
    The Superuser app can control how su is used.

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -135,6 +135,8 @@ static const struct fs_path_config android_files[] = {
     { 00750, AID_ROOT,      AID_SHELL,     0, "data/nativetest64/*" },
 
     /* CM's daemonized su doesn't need the setuid bit */
+    /* the following two files are INTENTIONALLY set-uid, but they
+     * are NOT included on user builds. */
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/xbin/su" },
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/procmem" },
     { 04750, AID_ROOT,      AID_RADIO,     0, "system/bin/pppd" },
