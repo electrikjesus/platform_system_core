3b02e4a3bdff83d539d2a453490e37ea8a0e9b5d
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-08-26 01:02:30 +0800

    ueventd: auto load modules on uevents
    This is a squashed and refactory patch of the following commits
    from marshmallow-x86:
    
     *  add modprobe-like capability and automatic ueventd loading
    
        Author: Jianxun Zhang <jianxun.zhang@intel.com>
        Author: Daniel Leung <daniel.leung@intel.com>
        Author: Andrew Boie <andrew.p.boie@intel.com>
    
        - insmod_by_dep() added to libcutils; loads a module into kernel.
        Modules the target module depends on will be loaded first. Loading
        will be stopped when an error occurs.
    
        - rmmod_by_dep() added to libcutils; removes a module from kernel.
        It also tries to remove other modules the target module depends
        on until an error occurs.
    
        - Implement wildcard matching for ueventd rules.
        The PCI and USB addresses for devices can change from devices
        from devices for a particular class of peripheral, for example,
        bluetooth. The ueventd rules created with these addresses are
        then device-specific.
    
        This changes the way ueventd rules with wildcard are handled.
        Instead of matching just the prefix with a trailing wildcard,
        now these rules can have wildcard anywhere in the rule.
        The wildcard matching is implemented using fnmatch(), where
        its matching is simliar to shell pathname expansion. It suits
        this particular usage model well.
    
        For example, instead of creating a rule to match:
    
          /sys/devices/pci0000:00/0000:00:1d.0/usb2/2-1/2-1.3/2-1.3:1.0/bluetooth/hci0/rfkill*
    
        , this would suffice:
    
          /sys/devices/*/bluetooth/hci0/rfkill*
    
        - Let ueventd auto-load kernel modules. Implements the functionality
        for ueventd to auto-load kernel modules when uevents are triggered.
        Since /system may not be mounted when uevents are fired,
        a deferred loading mechanism is implemented. Once mapping of
        module and alias is available, these modules are then loaded.
        Modules can also be blacklisted so they will not be loaded
        automatically. One example would be the Wifi driver, as
        Android's has to control its loading and unloading.
    
        - add 'probemod' builtin command. This command accepts the name of a
        kernel module plus a set of command line arguments. The module will
        be loaded, along with all its dependencies, using the libcutils
        insmod_by_dep() API.
    
        - Drivers in kernel can request modules by launching a program in
        user space, the program's path by default is "/sbin/modprobe".
        Because Android system has no modprobe and ueventd is the only
        program handling the module aliases so far, This patch provides a
        cheap approach to handle kernel's requests in ueventd executable.
    
        - Add new builtin init command "coldboot". The main purpose is to
        provide an approach in init.*.rc files to fire uevents for devices
        under the path which is passed as the the argument. This should be
        called after /system is mounted so any queued events that need to
        load a module can be fired.
    
     *  allow to load modules from standard module path
    
     *  ueventd: load modules in a forked process
    
        Loading some modules (e.g., ath3k) will be blocked to wait for
        firmware. The forking is necessary, otherwise ueventd has no
        chance to load the firmware.
    
     *  init: add modprobe to load kernel requested modules
    
     *  libcutils: refine probe_module
    
        Make the code be more elegant and fix the realloc bug.
    
     *  ueventd: refactory module and firmware loading mechanism
    
        Currently each module and firmware loading event is forked to
        a child process to handle. It may cause some race conditions.
    
        Now we fork ueventd into two processes. The parent process handles
        normal uevents including modules loading, while the child process
        only handles firmware loading events. All events are handled
        sequentially.
    
     *  ueventd: load all modules with the matched alias
    
        Originally we only load the first module with the matched alias.
        It causes some necessary modules are not loaded.

--- init/init.cpp
+++ init/init.cpp
@@ -546,6 +546,10 @@ static int charging_mode_booting(void) {
 }
 
 int main(int argc, char** argv) {
+    if (strstr(argv[0], "modprobe")) {
+        return modprobe_main(argc, argv);
+    }
+
     if (!strcmp(basename(argv[0]), "ueventd")) {
         return ueventd_main(argc, argv);
     }
